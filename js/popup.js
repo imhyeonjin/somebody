d926f8c9f80b5fdc5d78f5a77901e314f20fcf1d
imhyeonjin <98577241+imhyeonjin@users.noreply.github.com>
2022-01-28 17:58:02 +0900

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 $(function(){
   //$(".popup").hide();
-  $(".popup_btn a").click(function(){
+  $("#popup_btn a").click(function(){
     $(".popup").fadeIn(0);
   });
   $(".close").click(function(){
